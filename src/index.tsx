5cb0fb160c7dfb6b3d488a0320055704ad4e9e44
Q.S. Wang <wangqs_eclipse@yahoo.com>
2023-05-21 19:43:29 +1000

    fix: move list under visitor/appointment

--- src/index.tsx
+++ src/index.tsx
@@ -36,7 +36,7 @@ const router = createBrowserRouter([
     element: <VisitorAppointment />,
   },
   {
-    path: 'visitor/list',
+    path: 'visitor/appointment/list',
     element: <VisitorAppointment />,
   },
   {

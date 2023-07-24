3d7172a5bba5191c2a10de8347f44fcf2fd59e1d
Q.s <wangqs_eclipse@yahoo.com>
2023-07-24 15:46:37 +0800

    use single = in shell

--- scripts/build_harbor.sh
+++ scripts/build_harbor.sh
@@ -5,13 +5,9 @@ export BUILD_TIME=$(date +'%Y-%m-%dT%H:%M:%S%z')
 export DOCKER_DEFAULT_PLATFORM=linux/amd64
 export TAG=oci.tuxm.art:8443/tusdesign/html:${VERSION}
 
-echo ${TARGET_DEPLOYMENT}
-echo $1
-echo $2
-
 REACT_APP_BASE_URL="https://api-$TARGET_DEPLOYMENT.sz9wang.com/api/v1/public"
 
-if [ "$TARGET_DEPLOYMENT" == "prod" ]; then
+if [ "$TARGET_DEPLOYMENT" = "prod" ]; then
     REACT_APP_BASE_URL='https://api.sz9wang.com/api/v1/public'
 fi
 
